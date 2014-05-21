f148a54d8b734b87f7f4b4bca12a720b588f1cda
Carlos Romel Pereira da Silva <carlos.romel@gmail.com>
2014-05-20 21:25:23 -0300

    * Revisao do metodo combinatorio. Signed-off-by:Carlos Romel Pereira da Silva <carlos.romel@gmail.com>

--- NomesPossiveisHTML5/public_html/js/index.js
+++ NomesPossiveisHTML5/public_html/js/index.js
@@ -1,35 +1,41 @@
+function adicionar(campo, colecao, total) {
+    $(colecao).append($(campo).val() + "\n");
+    $(campo).val("");
+    $(campo).focus();
+    $(total).html($(colecao).val().split(/\n/).length);
+}
+
 $(function() {
     $("#nomes").append("Carlos Romel\n");
     $("#nomesMaternos").append("Freitas\n");
     $("#nomesMaternos").append("Correa\n");
     $("#nomesPaternos").append("Pereira\n");
     $("#nomesPaternos").append("da Silva\n");
+
     $("#btnAdicionarNome").click(function() {
-        $("#nomes").append($("#nome").val() + "\n");
-        $("#nome").val("");
-        $("#nome").focus();
+        adicionar("#nome", "#nomes", "#qtdNomes");
     });
 
     $("#btnAdicionarNomeMaterno").click(function() {
-        $("#nomesMaternos").append($("#nomeMaterno").val() + "\n");
-        $("#nomeMaterno").val("");
-        $("#nomeMaterno").focus();
+        adicionar("#nomeMaterno", "#nomesMaternos", "#qtdNomesMaternos");
     });
 
     $("#btnAdicionarNomePaterno").click(function() {
-        $("#nomesPaternos").append($("#nomePaterno").val() + "\n");
-        $("#nomePaterno").val("");
-        $("#nomePaterno").focus();
+        adicionar("#nomePaterno", "#nomesPaternos", "#qtdNomesPaternos");
     });
 
     $("#btnCombinar").click(function() {
+        var nomesGerados = "";
+        $("#nomesGerados").val("Teste\n");
         $.each($("#nomes").val().split(/\n/), function(n, nome) {
             $.each($("#nomesMaternos").val().split(/\n/), function(m, nomeMaterno) {
                 $.each($("#nomesPaternos").val().split(/\n/), function(p, nomePaterno) {
-                    var nomeGerado = nome + " " + nomeMaterno + " " + nomePaterno + "\n";
-                    $("#nomesGerados").append(nomeGerado);
+                    nomesGerados += nome + " " + nomeMaterno + " " + nomePaterno + "\n";
                 });
             });
         });
+
+        $("#nomesGerados").val(nomesGerados);
+        $("#qtdNomesGerados").html($("#nomesGerados").val().split(/\n/).length);
     });
 })
